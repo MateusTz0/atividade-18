983c61a8c9884511fd96e2acbec9ff6c86483f7d
MateusTz0 <cardoso_mateus1902@icloud.com>
2025-10-02 14:03:07 -0300

    usando as notas com dict

---
+++ ativ.py
@@ -0,0 +1,8 @@
+# Crie um dicionário chamado notas com as chaves: Matemática, Português, História. Atribua valores (notas) e exiba a média.
+
+notas = {"matematica": 8,
+         "portugues": 9,
+         "historia": 7}
+print(f"""      {notas["matematica"]},
+      {notas["portugues"]},
+      {notas["historia"]}""")
